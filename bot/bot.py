ef1adc335cd5dbf6b0bd05542a68dab754a7ea42
User <muso17091@gmail.com>
2025-04-26 09:53:00 +0500

    added session

--- bot/bot.py
+++ bot/bot.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-
+from aiogram.client.session.aiohttp import AiohttpSession
+session = AiohttpSession(proxy="http://proxy.server:3128")
 from handlers import router as handlers_router
 load_dotenv()
 
@@ -19,6 +20,7 @@ dp.include_router(handlers_router)
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    bot = Bot(token=TOKEN, session=session)
     await menu.set_bot_menu(bot)
     await dp.start_polling(bot)
 
